9670684fbc8488cf0034d919d3c2d3efb22b55d5
NeDeviant <arbuser0420@gmail.com>
2024-09-10 03:07:28 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('addButton').onclick = function() {
+  const input = document.getElementById('toDoInput'); // Получаем текст задачи
+  const newTask = input.value; // Сохраняем введенный текст
+
+  if (newTask) { // Если поле ввода не пустое
+      const li = document.createElement('li'); // Создаем новый элемент списка
+      li.textContent = newTask; // Устанавливаем текст задачи
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete task';
+      deleteButton.className = 'deleteButton';
+      deleteButton.onclick = function(){
+        li.remove();
+      }
+      li.appendChild(deleteButton);
+
+
+      document.getElementById('toDoLists').appendChild(li); // Добавляем задачу в список
+      input.value = ''; // Очищаем поле ввода
+  }
+};
